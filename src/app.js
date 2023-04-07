4c5b81cefe73827c0f5b8786de487a9ba4c72249
mclstb <103360094+mclstb@users.noreply.github.com>
2023-04-07 18:30:50 +0200

    date function corrected

--- src/app.js
+++ src/app.js
@@ -2,6 +2,31 @@ let apiKey = "a5e9d6baaddo9b42fdt0da22d43443d0";
 let units = "metric";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=${units}`;
 
+function formatDate(timestamp) {
+  //calculate date and return it
+  let date = new Date(timestamp);
+  console.log(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
   console.log(response.data);
   let currentTemperature = Math.round(response.data.temperature.current);
@@ -16,16 +41,7 @@ function showTemperature(response) {
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = formatDate(response.time * 1000).value;
-}
-
-function formatDate(timestamp) {
-  //calculate date and return it
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let day = date.getDay();
-  return `${day} ${hours}: ${minutes}`;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 axios.get(apiUrl).then(showTemperature);
