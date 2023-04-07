3bddc7bbeae349d1f24969d6279a2903ed48ccc9
mclstb <103360094+mclstb@users.noreply.github.com>
2023-04-07 17:54:46 +0200

    show current temperature function

---
+++ src/app.js
@@ -0,0 +1,20 @@
+let apiKey = "a5e9d6baaddo9b42fdt0da22d43443d0";
+let units = "metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=${units}`;
+
+function showTemperature(response) {
+  console.log(response.data);
+  let currentTemperature = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = `${currentTemperature}`;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  description.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+axios.get(apiUrl).then(showTemperature);
